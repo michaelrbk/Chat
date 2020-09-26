8e1b3e2ec4365dcf1dece3d163965277218c2ca7
Michael Becker <michaelbecker@grupotechnos.com.br>
2020-09-26 13:15:56 -0300

    refactor: database converted to sqlite for easier setup

--- ChatChallenge/ChatWebApp/Startup.cs
+++ ChatChallenge/ChatWebApp/Startup.cs
@@ -31,7 +31,7 @@ namespace ChatWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
